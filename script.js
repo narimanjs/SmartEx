34274f36097f5bc0e146474141dcb8ad2457114b
Nariman <mirmanov1@mail.ru>
2024-07-31 16:53:27 +0500

    test tasks are done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-console.log("hello SmartEx");
+console.log("Hello SmartEx Company!");
 
-//  Задача 1
+// !------------------------- Задача 1 -------------------------
 //  Исправьте код так, чтобы результатом выполнения функции logArrayInfo([1, 2, 3]) было:
 //  1
 //  2
@@ -34,10 +34,100 @@ async function logArrayInfo(array) {
 
 logArrayInfo([1, 2, 3]);
 
-// CONSOLE
-// ----------
-// "Done!"
-// 1;
-// 2;
-// 3;
-// */
+// !------------------------- Задача 2 -------------------------
+// Данный код создаст массив из котиков. Каждая функция в массиве должна вывести свой порядковый номер, но что то пошло не
+// так и все кошки говорят, что их номер 10. Почему у всех кошек одинаковые номера? Исправьте код, чтобы он работал как
+// задумано.
+
+function createCats() {
+  // Ошибка заключается в использовании переменной var для переменной i. Переменная var имеет функциональную область видимости, что означает, что все функции замыкаются на одной и той же переменной i, и после завершения цикла значение i становится 10. Для исправления этой проблемы необходимо использовать переменную let, которая имеет блочную область видимости.
+  let cats = [];
+  // var i = 0;
+  // 4 while (i < 10) {
+  // 5 let cat = function() {
+  // 6 console.log(`My index is ${i}`);
+  // 7 };
+  // Использование IIFE создает новую область видимости для каждой итерации цикла while. Это позволяет каждой функции замыкаться на правильное значение i.
+  let i = 0;
+  while (i < 10) {
+    (function (i) {
+      let cat = function () {
+        console.log(`My index is ${i}`);
+      };
+      cats.push(cat);
+    })(i);
+    i++;
+  }
+  return cats;
+
+  //! или можно еще использовать for
+  // for (let i = 0; i < 10; i++) {
+  //   let cat = function () {
+  //     console.log(`My index is ${i}`);
+  //   };
+  //   cats.push(cat);
+  // }
+
+  // return cats;
+}
+
+let animals = createCats();
+animals[0](); // My index is 0
+animals[5](); // My index is 5
+
+// ! ------------------------- Задача 3 -------------------------
+// Необходимо найти сумму всех вершин, значение которых кратно 2
+const tree = {
+  value: 3,
+  children: [
+    {
+      value: 1,
+      children: [],
+    },
+    {
+      value: 4,
+      children: [],
+    },
+    {
+      value: 3,
+      children: [
+        {
+          value: 8,
+          children: [
+            {
+              value: 2,
+              children: [],
+            },
+            {
+              value: 5,
+              children: [],
+            },
+          ],
+        },
+        {
+          value: 0,
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+function sumOfEvenVertices(node) {
+  if (!node) return 0;
+
+  let sum = 0;
+  if (node.value % 2 === 0) {
+    sum += node.value;
+  }
+
+  for (let child of node.children) {
+    sum += sumOfEvenVertices(child);
+  }
+
+  return sum;
+}
+
+let sum = sumOfEvenVertices(tree);
+console.log(sum);
+
+//
